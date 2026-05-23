203a7a4a5246709296307aea01b1c28a12cd96fa
alex <alex.podikoglou@gmail.com>
2026-05-24 02:36:56 +0300

    docs: add module doc to runtime/proto.lua

--- src/ljs/runtime/proto.lua
+++ src/ljs/runtime/proto.lua
@@ -1,3 +1,6 @@
+-- Forward declarations of the root prototype tables. These are populated later
+-- by the runtime stdlib files (object.lua, function.lua). Declared first so that
+-- all helpers (which run between proto and stdlib in the preamble) can reference them.
 local _ljs_object_prototype = {}
 
 local _ljs_function_prototype = {}
