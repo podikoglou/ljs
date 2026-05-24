1f107967f28b065d6597b2165aebe5a353881c09
alex <alex.podikoglou@gmail.com>
2026-05-24 11:40:34 +0300

    feat(runtime): add Boolean prototype with toString/valueOf and constructor

---
+++ src/ljs/runtime/boolean.lua
@@ -0,0 +1,27 @@
+_ljs_boolean_prototype.toString = _ljs_fn(function(_ljs_this)
+  if _ljs_this._ljs_data then
+    return "true"
+  end
+  return "false"
+end)
+_ljs_boolean_prototype.valueOf = _ljs_fn(function(_ljs_this)
+  return _ljs_this._ljs_data
+end)
+
+setmetatable(_ljs_boolean_prototype, { __index = _ljs_object_prototype })
+
+local Boolean = _ljs_fn(function(_ljs_this, value)
+  if value == nil then
+    value = false
+  end
+  if type(value) ~= "boolean" then
+    value = value ~= false and value ~= nil
+  end
+  if _ljs_this == nil then
+    return value
+  end
+  _ljs_this._ljs_data = value
+  return _ljs_this
+end)
+Boolean.prototype = _ljs_boolean_prototype
+_ljs_boolean_prototype.constructor = Boolean
